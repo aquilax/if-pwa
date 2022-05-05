b97368bbbcfeedb86a907ac6bda72ea7624aabb5
aquilax <aquilax@gmail.com>
2022-05-05 17:12:41 +0200

    Fix service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  globDirectory: ".",
+  globDirectory: "./dist",
   globPatterns: ["**/*.{png,html,js,json}"],
   swDest: "dist/sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
